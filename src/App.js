c9c536fb49fba1d39173ec85c8c846da31c860a0
irplagura <i.rplagura@gmail.com>
2023-01-07 10:24:33 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      score: 0
-    };
-  }
-
-  incrementScore = () => {
-    this.setState({
-      score: this.state.score + 1
-    });
-  }
-
-  render() {
-    return (
-      <div className="game">
-        <h1 className="score">Score: {this.state.score}</h1>
-        <button onClick={this.incrementScore}>Increment</button>
-      </div>
-    );
-  }
-}
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.<br></br>
+	      <b>Ray is Learning docker and kubernetes, yay!</b>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
